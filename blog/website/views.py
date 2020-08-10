52fae8e399d4601ad96beeacb52c31db305f1041
Mayara Nascimento <maybey1204@gmail.com>
2020-08-10 18:34:11 -0300

    Lendo posts do Banco de dados

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def hello_blog(request):
     lista = ['Django', 'Python', 'Git', 'Html',
              'Banco de dados', 'Linux', 'Nginx', 'Uwsgi',
               'Systemctl'
-             ]
-    data = {'name': 'Curso Django 3', 'lista_tecnologia': lista}
+    ]
+    list_post = Post.objects.all()
+
+    data = {
+        'name': 'Curso Django 3',
+        'lista_tecnologia': lista,
+        'posts': list_post,
+    }
+
     return render(request, 'index.html', data)
